d01a22fd6984c9c21b297d480660292e80f02264
Joseph DeRose <jderose@dhcp-visitor-216-211.slac.stanford.edu>
2018-05-11 13:37:50 -0700

    Including luminosity function in rdelmodel because needed for cdf computation

--- PyAddgals/addgalsModel.py
+++ PyAddgals/addgalsModel.py
@@ -1,13 +1,16 @@
 from __future__ import print_function, division
+from scipy.special import erf
+import numpy as np
 
 from .galaxyModel import GalaxyModel
 from . import luminosityFunction
 
+
 class ADDGALSModel(GalaxyModel):
 
     def __init__(self, nbody, luminosityFunctionConfig=None,
-                    rdelModelConfig=None,
-                    redFractionModelConfig=None, colorModelConfig=None):
+                 rdelModelConfig=None,
+                 redFractionModelConfig=None, colorModelConfig=None):
 
         self.nbody = nbody
 
@@ -23,15 +26,15 @@ class ADDGALSModel(GalaxyModel):
         if colorModelConfig is None:
             raise(ValueError('ADDGALS model must define colorModelConfig'))
 
-        self.rdelModel = RdelModel(**rdelModelConfig)
-        self.redFractionModel = RedFractionModel(**redFractionModelConfig)
-        self.colorModel = ColorModel(**colorModelConfig)
-
         lf_type = luminosityFunctionConfig.pop('modeltype')
 
         self.luminosityFunction = getattr(luminosityFunction, lf_type)
-        self.luminosityFunction = self.luminosityFunction(nbody.cosmo, **luminosityFunctionConfig)
+        self.luminosityFunction = self.luminosityFunction(
+            nbody.cosmo, **luminosityFunctionConfig)
 
+        self.rdelModel = RdelModel(self.luminosityFunction, **rdelModelConfig)
+        self.redFractionModel = RedFractionModel(**redFractionModelConfig)
+        self.colorModel = ColorModel(**colorModelConfig)
 
     def paintGalaxies(self):
         """Paint galaxies into nbody using ADDGALS
@@ -49,21 +52,21 @@ class ADDGALSModel(GalaxyModel):
         domain = self.nbody.domain
 
         n_gal = self.luminosityFunction.integrate(domain.zmin,
-                                                    domain.zmax,
-                                                    domain.getArea())
+                                                  domain.zmax,
+                                                  domain.getArea())
 
         z = self.drawRedshifts(n_gal)
         mag = self.luminosityFunction.sampleLuminosities(domain, z)
         dens = self.rdelModel.sampleDensities(z, mag)
-
 
         self.catalog = None
 
 
 class RdelModel(object):
 
-    def __init__(self, modelfile=None, **kwargs):
+    def __init__(self, lf, modelfile=None, **kwargs):
 
+        self.luminosityFunction = lf
         self.modelfile = modelfile
 
         if self.modelfile is None:
