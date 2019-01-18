c903772e48772ec9aea18c920a04e49e4802a8f3
Joe DeRose <joe.derose13@gmail.com>
2019-01-18 15:46:32 -0800

    update file open

--- bin/skyfactory/merge_buzzard.py
+++ bin/skyfactory/merge_buzzard.py
@@ -476,11 +476,11 @@ class buzzard_flat_cat(object):
                    'zmean_sof': 'mean-z'}
 
         gout = h5py.File(self.odir + '/' + self.simname +
-                         '_{}'.format(self.obsdir[:-1]) + '_gold.h5', 'rw')
+                         '_{}'.format(self.obsdir[:-1]) + '_gold.h5', 'w')
         sout = h5py.Fits(self.odir + '/' + self.simname +
-                         '_{}'.format(self.obsdir[:-1]) + '_shape.h5', 'rw')
+                         '_{}'.format(self.obsdir[:-1]) + '_shape.h5', 'w')
         pout = h5py.Fits(self.odir + '/' + self.simname +
-                         '_{}'.format(self.obsdir[:-1]) + '_bpz.h5', 'rw')
+                         '_{}'.format(self.obsdir[:-1]) + '_bpz.h5', 'w')
 
         gfiles = glob.glob(self.odir + '/' +
                            self.simname + '_{}'.format(self.obsdir[:-1]) +
