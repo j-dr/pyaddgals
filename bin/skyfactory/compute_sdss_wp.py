168bf84098e72ddbb936e07956dddae43ecfb14d
Joe DeRose <joe.derose13@gmail.com>
2019-07-31 13:58:24 -0700

    typecasing

--- bin/skyfactory/compute_sdss_wp.py
+++ bin/skyfactory/compute_sdss_wp.py
@@ -337,7 +337,7 @@ if __name__ == '__main__':
 
     model = ADDGALSModel(nbody, **config['GalaxyModel']['ADDGALSModel'])
 
-    for i, f in enumerate(files):
+    for i, f in enumerate(files[:2]):
         print(i)
         sys.stdout.flush()
         gi = fitsio.read(f, columns=['PX', 'PY', 'PZ', 'TRA', 'TDEC',
