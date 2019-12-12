3b2523003782ae6998af823f619741fb4d4b816a
Joe DeRose <joe.derose13@gmail.com>
2019-12-12 13:59:16 -0800

    updated to read in raw observed files rather than h5

--- bin/skyfactory/make_dnf_training_set.py
+++ bin/skyfactory/make_dnf_training_set.py
@@ -1,4 +1,4 @@
-import h5py as h5
+from glob import glob
 import numpy as np
 import fitsio
 import sys
@@ -6,26 +6,32 @@ import sys
 
 if __name__ == '__main__':
 
-    mastercatfile = sys.argv[1]
+    inglob = sys.argv[1]
     outfile = sys.argv[2]
 
-    mcat = h5.File(mastercatfile, 'r')
+    infiles = glob(inglob)
+    for i, f in enumerate(infiles):
 
-    training_columns_gold = dict(zip(['ID', 'RA', 'DEC', 'FLUX_G', 'FLUX_R', 'FLUX_I', 'FLUX_Z', 'MAGERR_G', 'MAGERR_R', 'MAGERR_I', 'MAGERR_Z'],
-                                     ['coadd_object_id', 'ra', 'dec', 'flux_g', 'flux_r', 'flux_i', 'flux_z', 'magerr_g', 'magerr_r', 'magerr_i', 'magerr_z']))
-    training_columns_z = dict(zip(['Z', 'Z_COS'], ['z', 'redshift_cos']))
+        g = fitsio.read(f, columns=['ID', 'RA', 'DEC', 'FLUX_G', 'FLUX_R', 'FLUX_I', 'FLUX_Z',
+                                    'MAGERR_G', 'MAGERR_R', 'MAGERR_I', 'MAGERR_Z', 'Z', 'Z_COS',
+                                    'MAG_I'])
 
-    mag_i = mcat['catalog/gold/mag_i'][:]
-    idx = np.where(mag_i < 23)
-    idx = np.random.choice(idx, size=len(idx) // 100)
+        idx = g['MAG_I'] < 23
+        idx = np.random.choice(idx, size=len(idx) // 100)
+        g = g[idx]
 
-    train = np.zeros(len(idx), dtype=np.dtype([('ID', '>i8'), ('RA', '>f8'), ('DEC', '>f8'),
-                                               ('FLUX_G', '>f8'), ('FLUX_R', '>f8'), ('FLUX_I', '>f8'), ('FLUX_Z', '>f8'), ('MAGERR_G', '>f8'), ('MAGERR_R', '>f8'), ('MAGERR_I', '>f8'), ('MAGERR_Z', '>f8'), ('Z', '>f8'), ('MAG_I', '>f8'), ('Z_COS', '>f8')]))
+        train = np.zeros(len(idx), dtype=np.dtype([('ID', '>i8'), ('RA', '>f8'), ('DEC', '>f8'),
+                                                   ('FLUX_G', '>f8'), ('FLUX_R', '>f8'), ('FLUX_I', '>f8'),
+                                                   ('FLUX_Z', '>f8'), ('MAGERR_G', '>f8'), ('MAGERR_R', '>f8'),
+                                                   ('MAGERR_I', '>f8'), ('MAGERR_Z', '>f8'), ('Z', '>f8'),
+                                                   ('MAG_I', '>f8'), ('Z_COS', '>f8')]))
 
-    for k in training_columns_gold.keys():
-        train[k] = mcat['catalog/gold/{}'.format(training_columns_gold[k])][:][idx]
+        for k in train.dtype.names:
+            train[k] = g[k]
 
-    for k in training_columns_z.keys():
-        train[k] = mcat['catalog/bpz/unsheared/{}'.format(training_columns_z[k])][:][idx]
-
-    fitsio.write(outfile, train)
+        if i == 0:
+            out = fitsio.FITS(outfile)
+            out.write(train)
+        else:
+            out = fitsio.FITS(outfile)
+            out[-1].append(train)
