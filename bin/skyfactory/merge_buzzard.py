6b99cba8fd1cd16c719d03c98b68b3aa556b8fa5
Joe DeRose <joe.derose13@gmail.com>
2020-03-04 19:23:45 -0800

    including mcal fluxes in master catalog

--- bin/skyfactory/merge_buzzard.py
+++ bin/skyfactory/merge_buzzard.py
@@ -47,389 +47,8 @@ class buzzard_flat_cat(object):
         self.already_merged = already_merged
         self.odir = self.obsdir
 
-        if loop:
-            self.loop_cats(debug=debug)
-
         if merge:
             self.merge_rank_files(merge_with_bpz=merge_with_bpz)
-
-    def loop_cats(self, debug=False):
-
-        if debug:
-            gold = np.zeros(self.maxrows, dtype=[('coadd_objects_id', 'i8')]
-                            + [('ra', 'f4')]
-                            + [('dec', 'f4')]
-                            + [('redshift', 'f4')]
-                            + [('mag_g', 'f4')]
-                            + [('mag_r', 'f4')]
-                            + [('mag_i', 'f4')]
-                            + [('mag_z', 'f4')]
-                            + [('ivar_g', 'f4')]
-                            + [('ivar_r', 'f4')]
-                            + [('ivar_i', 'f4')]
-                            + [('ivar_z', 'f4')]
-                            + [('flags_badregion', 'i8')]
-                            + [('flags_gold', 'i8')]
-                            + [('hpix', 'i8')]
-                            + [('lss-sample', 'i8')]
-                            + [('wl-sample', 'i8')])
-
-        else:
-            gold = np.zeros(self.maxrows, dtype=[('coadd_objects_id', 'i8')]
-                            + [('ra', 'f4')]
-                            + [('dec', 'f4')]
-                            + [('mag_r', 'f4')]
-                            + [('redshift', 'f4')]
-                            + [('flags_badregion', 'i8')]
-                            + [('flags_gold', 'i8')]
-                            + [('hpix', 'i8')]
-                            + [('lss-sample', 'i8')]
-                            + [('wl-sample', 'i8')])
-
-        if debug:
-            shape = np.zeros(self.maxrows, dtype=[('coadd_objects_id', 'i8')]
-                             + [('ra', 'f4')]
-                             + [('dec', 'f4')]
-                             + [('e1', 'f4')]
-                             + [('e2', 'f4')]
-                             + [('g1', 'f4')]
-                             + [('g2', 'f4')]
-                             + [('kappa', 'f4')]
-                             + [('m1', 'f4')]
-                             + [('m2', 'f4')]
-                             + [('c1', 'f4')]
-                             + [('c2', 'f4')]
-                             + [('weight', 'f4')]
-                             + [('size', 'f4')]
-                             + [('flags', 'i8')])
-        else:
-            shape = np.zeros(self.maxrows, dtype=[('coadd_objects_id', 'i8')]
-                             + [('ra', 'f4')]
-                             + [('dec', 'f4')]
-                             + [('e1', 'f4')]
-                             + [('e2', 'f4')]
-                             + [('g1', 'f4')]
-                             + [('g2', 'f4')]
-                             + [('kappa', 'f4')]
-                             + [('m1', 'f4')]
-                             + [('m2', 'f4')]
-                             + [('c1', 'f4')]
-                             + [('c2', 'f4')]
-                             + [('weight', 'f4')]
-                             + [('flags', 'i8')])
-
-        photoz = np.zeros(self.maxrows, dtype=[('coadd_objects_id', 'i8')]
-                          + [('mean-z', 'f8')]
-                          + [('mc-z', 'f8')]
-                          + [('mode-z', 'f8')]
-                          + [('redshift', 'f8')]
-                          + [('weight', 'f8')]
-                          + [('flags', 'f8')])
-
-        lenst = 0
-
-        for ifile, filename in enumerate(glob.glob(self.rootdir + '/' + self.obsdir + '*' + self.obsname + '*[0-9].fits')):
-
-            gout = fio.FITS(
-                self.simname + '_{}'.format(self.obsdir[:-1]) + '_gold.fits', 'rw')
-            sout = fio.FITS(
-                self.simname + '_{}'.format(self.obsdir[:-1]) + '_shape.fits', 'rw')
-            pout = fio.FITS(
-                self.simname + '_{}'.format(self.obsdir[:-1]) + '_pz.fits', 'rw')
-
-            tname = filename.replace(self.obsname, self.truthname).replace(
-                self.obsdir, self.truthdir)
-            pzname = filename.replace(self.obsname, self.pzname).replace(
-                self.obsdir, self.pzdir).replace('.fits', '.BPZ.fits')
-
-            truth = fio.FITS(
-                tname)[-1].read(columns=['ID', 'GAMMA1', 'GAMMA2', 'KAPPA', 'Z'])
-            if not debug:
-                obs = fio.FITS(filename)[-1].read(columns=['RA', 'DEC',
-                                                           'EPSILON1', 'EPSILON2', 'LSS_FLAG', 'WL_FLAG', 'MAG_R'])
-            else:
-                obs = fio.FITS(filename)[-1].read(columns=['RA', 'DEC', 'EPSILON1', 'EPSILON2', 'LSS_FLAG',
-                                                           'WL_FLAG', 'MAG_G', 'MAG_R', 'MAG_I', 'MAG_Z', 'IVAR_G', 'IVAR_R', 'IVAR_I', 'IVAR_Z', 'SIZE'])
-
-            pz = fio.FITS(
-                pzname)[-1].read(columns=['MEAN_Z', 'Z_MC', 'MODE_Z'])
-
-            sflag = np.zeros(len(obs), dtype=int)
-            sflag[obs['LSS_FLAG'] == 1] = 1
-            sflag[obs['WL_FLAG'] == 1] = 2
-
-            if self.nzcut:
-                sflag[obs['MAG_R'] > (
-                    20.8755386 + 2.88949793 * truth['Z'])] = 0
-
-            truth = truth[sflag > 0]
-            obs = obs[sflag > 0]
-            pz = pz[sflag > 0]
-            sflag = sflag[sflag > 0]
-
-            # insert selection function here to mask truth/obs (if can be run on individual files)
-
-            print(ifile, len(truth), filename)
-
-            gold['coadd_objects_id'][lenst:lenst + len(truth)] = truth['ID']
-            gold['ra'][lenst:lenst + len(truth)] = obs['RA']
-            gold['dec'][lenst:lenst + len(truth)] = obs['DEC']
-            gold['redshift'][lenst:lenst + len(truth)] = truth['Z']
-            gold['hpix'][lenst:lenst + len(truth)] = hp.ang2pix(
-                16384, np.pi / 2. - np.radians(obs['DEC']), np.radians(obs['RA']), nest=True)
-            gold['lss-sample'][lenst:lenst + len(truth)] = obs['LSS_FLAG']
-            gold['wl-sample'][lenst:lenst + len(truth)] = obs['WL_FLAG']
-            gold['mag_r'][lenst:lenst + len(truth)] = obs['MAG_R']
-
-            if debug:
-                gold['mag_g'][lenst:lenst + len(truth)] = obs['MAG_G']
-                gold['mag_i'][lenst:lenst + len(truth)] = obs['MAG_I']
-                gold['mag_z'][lenst:lenst + len(truth)] = obs['MAG_Z']
-                gold['ivar_g'][lenst:lenst + len(truth)] = obs['IVAR_G']
-                gold['ivar_r'][lenst:lenst + len(truth)] = obs['IVAR_R']
-                gold['ivar_i'][lenst:lenst + len(truth)] = obs['IVAR_I']
-                gold['ivar_z'][lenst:lenst + len(truth)] = obs['IVAR_Z']
-
-            shape['ra'][lenst:lenst + len(truth)] = truth['RA']
-            shape['dec'][lenst:lenst + len(truth)] = truth['DEC']
-            shape['coadd_objects_id'][lenst:lenst + len(truth)] = truth['ID']
-            shape['e1'][lenst:lenst + len(truth)] = obs['EPSILON1']
-            shape['e2'][lenst:lenst + len(truth)] = obs['EPSILON2']
-            shape['g1'][lenst:lenst + len(truth)] = truth['GAMMA1']
-            shape['g2'][lenst:lenst + len(truth)] = truth['GAMMA2']
-            shape['kappa'][lenst:lenst + len(truth)] = truth['KAPPA']
-            shape['m1'][lenst:lenst + len(truth)] += 1.
-            shape['m2'][lenst:lenst + len(truth)] += 1.
-            shape['weight'][lenst:lenst + len(truth)] += 1.
-            if debug:
-                shape['size'][lenst:lenst + len(truth)] = obs['SIZE']
-
-            photoz['coadd_objects_id'][lenst:lenst + len(truth)] = truth['ID']
-            photoz['mean-z'][lenst:lenst + len(truth)] = pz['MEAN_Z']
-            photoz['mc-z'][lenst:lenst + len(truth)] = pz['Z_MC']
-            photoz['mode-z'][lenst:lenst + len(truth)] = pz['MODE_Z']
-            photoz['redshift'][lenst:lenst + len(truth)] = truth['Z']
-            photoz['weight'][lenst:lenst + len(truth)] += 1.
-
-            if ifile == 0:
-                gout.write(gold[lenst:lenst + len(truth)])
-                sout.write(shape[lenst:lenst + len(truth)])
-                pout.write(photoz[lenst:lenst + len(truth)])
-            else:
-                gout[-1].append(gold[lenst:lenst + len(truth)])
-                sout[-1].append(shape[lenst:lenst + len(truth)])
-                pout[-1].append(photoz[lenst:lenst + len(truth)])
-
-            sout.close()
-            pout.close()
-            gout.close()
-
-            lenst += len(truth)
-
-        return
-
-    def process_single_file(self, obs, truth, pz, sample, rank, debug=False):
-
-        if debug:
-            gold = np.zeros(len(obs), dtype=[('coadd_objects_id', 'i8')]
-                            + [('ra', 'f4')]
-                            + [('dec', 'f4')]
-                            + [('redshift', 'f4')]
-                            + [('mag_g', 'f4')]
-                            + [('mag_r', 'f4')]
-                            + [('mag_i', 'f4')]
-                            + [('mag_z', 'f4')]
-                            + [('magerr_g', 'f4')]
-                            + [('magerr_r', 'f4')]
-                            + [('magerr_i', 'f4')]
-                            + [('magerr_z', 'f4')]
-                            + [('ivar_g', 'f4')]
-                            + [('ivar_r', 'f4')]
-                            + [('ivar_i', 'f4')]
-                            + [('ivar_z', 'f4')]
-                            + [('flags_badregion', 'i8')]
-                            + [('flags_gold', 'i8')]
-                            + [('hpix', 'i8')]
-                            + [('lss-sample', 'i8')]
-                            + [('wl-sample', 'i8')])
-
-        else:
-            gold = np.zeros(len(obs), dtype=[('coadd_objects_id', 'i8')]
-                            + [('ra', 'f4')]
-                            + [('dec', 'f4')]
-                            + [('mag_r', 'f4')]
-                            + [('redshift', 'f4')]
-                            + [('flags_badregion', 'i8')]
-                            + [('flags_gold', 'i8')]
-                            + [('hpix', 'i8')]
-                            + [('lss-sample', 'i8')]
-                            + [('wl-sample', 'i8')])
-
-        if debug:
-            shape = np.zeros(len(obs), dtype=[('coadd_objects_id', 'i8')]
-                             + [('ra', 'f4')]
-                             + [('dec', 'f4')]
-                             + [('e1', 'f4')]
-                             + [('e2', 'f4')]
-                             + [('g1', 'f4')]
-                             + [('g2', 'f4')]
-                             + [('kappa', 'f4')]
-                             + [('m1', 'f4')]
-                             + [('m2', 'f4')]
-                             + [('c1', 'f4')]
-                             + [('c2', 'f4')]
-                             + [('weight', 'f4')]
-                             + [('size', 'f4')]
-                             + [('flags', 'i8')])
-        else:
-            shape = np.zeros(len(obs), dtype=[('coadd_objects_id', 'i8')]
-                             + [('ra', 'f4')]
-                             + [('dec', 'f4')]
-                             + [('e1', 'f4')]
-                             + [('e2', 'f4')]
-                             + [('g1', 'f4')]
-                             + [('g2', 'f4')]
-                             + [('kappa', 'f4')]
-                             + [('m1', 'f4')]
-                             + [('m2', 'f4')]
-                             + [('c1', 'f4')]
-                             + [('c2', 'f4')]
-                             + [('weight', 'f4')]
-                             + [('flags', 'i8')])
-
-#        altlens = np.zeros(dtype=[('coadd_objects_id', 'i8')]
-#                           + [('ra', 'f4')]
-#                           + [('dec', 'f4')]
-#                           + [('mag_i', 'f4')]
-#                           + [('magerr_i', 'f4')]
-#                           + [('flux_i', 'f4')]
-#                           + [('ivar_i', 'f4')]
-#                           + [('weight', 'f4')]
-#                           + [('flags', 'i8')])
-
-        photoz = np.zeros(len(obs), dtype=[('coadd_objects_id', 'i8')]
-                          + [('mean-z', 'f8')]
-                          + [('mc-z', 'f8')]
-                          + [('mode-z', 'f8')]
-                          + [('redshift', 'f8')]
-                          + [('weight', 'f8')]
-                          + [('z_cos'), 'f4']
-                          + [('flags', 'f8')])
-
-        lenst = 0
-
-        if os.path.exists(self.odir + '/' + self.simname + '_{}'.format(self.obsdir[:-1]) + '_gold.{}.fits'.format(rank)):
-            ifile = 1
-        elif not os.path.exists(self.odir):
-            try:
-                os.makedirs(self.odir)
-            except:
-                pass
-            ifile = 0
-        else:
-            ifile = 0
-
-        gout = fio.FITS(self.odir + '/' + self.simname +
-                        '_{}'.format(self.obsdir[:-1]) + '_gold.{}.fits'.format(rank), 'rw')
-        sout = fio.FITS(self.odir + '/' + self.simname +
-                        '_{}'.format(self.obsdir[:-1]) + '_shape.{}.fits'.format(rank), 'rw')
-        pout = fio.FITS(self.odir + '/' + self.simname +
-                        '_{}'.format(self.obsdir[:-1]) + '_pz.{}.fits'.format(rank), 'rw')
-#        alout = fio.FITS(self.odir + '/' + self.simname +
-#                         '_{}'.format(self.obsdir[:-1]) + '_alt_lens.{}.fits'.format(rank), 'rw')
-
-        sflag = np.zeros(len(obs), dtype=int)
-        sflag[sample['LSS_FLAG'] == 1] = 1
-        sflag[sample['WL_FLAG'] == 1] = 2
-
-#        if self.nzcut:
-#            sflag[obs['MAG_R'] > (20.8755386 + 2.88949793 * truth['Z'])] = 0
-
-        truth = truth[sflag > 0]
-        obs = obs[sflag > 0]
-        if pz is not None:
-            pz = pz[sflag > 0]
-
-        sample = sample[sflag > 0]
-
-        if self.nzcut:
-            gold = gold[sflag > 0]
-            shape = shape[sflag > 0]
-            photoz = photoz[sflag > 0]
-
-        # insert selection function here to mask truth/obs (if can be run on individual files)
-
-        gold['coadd_objects_id'] = truth['ID']
-        gold['ra'] = obs['RA']
-        gold['dec'] = obs['DEC']
-        gold['redshift'] = truth['Z']
-
-        gold['hpix'] = hp.ang2pix(
-            16384, np.pi / 2. - np.radians(obs['DEC']), np.radians(obs['RA']), nest=True)
-        gold['lss-sample'] = sample['LSS_FLAG']
-        gold['wl-sample'] = sample['WL_FLAG']
-        gold['mag_r'] = obs['MAG_R']
-
-        if debug:
-            gold['mag_g'] = obs['MAG_G']
-            gold['mag_i'] = obs['MAG_I']
-            gold['mag_z'] = obs['MAG_Z']
-            gold['ivar_g'] = obs['IVAR_G']
-            gold['ivar_r'] = obs['IVAR_R']
-            gold['ivar_i'] = obs['IVAR_I']
-            gold['ivar_z'] = obs['IVAR_Z']
-
-        shape['coadd_objects_id'] = truth['ID']
-        shape['ra'] = obs['RA']
-        shape['dec'] = obs['DEC']
-        shape['e1'] = obs['EPSILON1']
-        shape['e2'] = obs['EPSILON2']
-        shape['g1'] = truth['GAMMA1']
-        shape['g2'] = truth['GAMMA2']
-        shape['kappa'] = truth['KAPPA']
-        shape['m1'] += 1.
-        shape['m2'] += 1.
-        shape['weight'] += 1.
-        if debug:
-            shape['size'] = obs['SIZE']
-
-#        altlens['coadd_objects_id'] = truth['ID']
-#        altlens['ra'] = obs['RA']
-#        altlens['dec'] = obs['DEC']
-#        altlens['mag_i'] = obs['MAG_I']
-#        altlens['magerr_i'] = obs['MAGERR_I']
-#        altlens['flux_i'] = obs['FLUX_I']
-#        altlens['ivar_i'] = obs['IVAR_I']
-#        altlens['weight'] = 1
-#        altlens['flags'] = 0
-
-        photoz['coadd_objects_id'] = truth['ID']
-        if pz is not None:
-            photoz['mean-z'] = pz['MEAN_Z']
-            photoz['mc-z'] = pz['Z_MC']
-            photoz['mode-z'] = pz['MODE_Z']
-
-        photoz['redshift'] = truth['Z']
-        photoz['z_cos'] = truth['Z_COS']
-        photoz['weight'] += 1.
-
-        if ifile == 0:
-            gout.write(gold)
-            sout.write(shape)
-            pout.write(photoz)
-#            alout.write(altlens)
-        else:
-            gout[-1].append(gold)
-            sout[-1].append(shape)
-            pout[-1].append(photoz)
-#            alout[-1].append(altlens)
-
-        sout.close()
-        pout.close()
-        gout.close()
-#        alout.close()
-
-        return
 
     def process_single_file_all_detections(self, obs, truth, pz, rank, debug=False):
 
@@ -469,10 +88,16 @@ class buzzard_flat_cat(object):
                         + [('flux_r', 'f4')]
                         + [('flux_i', 'f4')]
                         + [('flux_z', 'f4')]
+                        + [('mcal_flux_r', 'f4')]
+                        + [('mcal_flux_i', 'f4')]
+                        + [('mcal_flux_z', 'f4')]
                         + [('ivar_g', 'f4')]
                         + [('ivar_r', 'f4')]
                         + [('ivar_i', 'f4')]
                         + [('ivar_z', 'f4')]
+                        + [('mcal_ivar_r', 'f4')]
+                        + [('mcal_ivar_i', 'f4')]
+                        + [('mcal_ivar_z', 'f4')]
                         + [('sdss_sedid', 'i8')]
                         + [('flags_badregion', 'i8')]
                         + [('flags_gold', 'i8')]
@@ -562,10 +187,18 @@ class buzzard_flat_cat(object):
         gold['flux_i'] = obs['FLUX_I']
         gold['flux_z'] = obs['FLUX_Z']
 
+        gold['mcal_flux_r'] = obs['MCAL_FLUX_R']
+        gold['mcal_flux_i'] = obs['MCAL_FLUX_I']
+        gold['mcal_flux_z'] = obs['MCAL_FLUX_Z']
+
         gold['ivar_g'] = obs['IVAR_G']
         gold['ivar_r'] = obs['IVAR_R']
         gold['ivar_i'] = obs['IVAR_I']
         gold['ivar_z'] = obs['IVAR_Z']
+
+        gold['mcal_ivar_r'] = obs['MCAL_IVAR_R']
+        gold['mcal_ivar_i'] = obs['MCAL_IVAR_I']
+        gold['mcal_ivar_z'] = obs['MCAL_IVAR_Z']
 
         gold['rhalo'] = truth['RHALO']
         gold['r200'] = truth['R200']
