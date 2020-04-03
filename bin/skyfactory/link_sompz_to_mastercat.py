b8a056d5f9ae3a1f3d385e70efefcddbcd721761
Joe DeRose <joe.derose13@gmail.com>
2020-04-03 16:42:55 -0700

    updated sompz linking to skip sorting if not necessary

--- bin/skyfactory/link_sompz_to_mastercat.py
+++ bin/skyfactory/link_sompz_to_mastercat.py
@@ -2,64 +2,74 @@ import numpy as np
 import h5py as h5
 import sys
 
-def include_sompz(catfile, sompzfile, bpzfile):
 
-    with h5.File(sompzfile, 'r') as fs:
+def include_sompz(catfile, sompzfile, bpzfile, sort=False):
 
-        som_id = fs['catalog/sompz/unsheared/coadd_object_id'][:]
-        cols = list(fs['catalog/sompz/unsheared/'].keys())
-        
-        print(cols)
-        sys.stdout.flush()
+    if not sort:
+        with h5.File(catfile) as f:
+            f['catalog/sompz'] = h5.ExternalLink(sompzfile, "/catalog/sompz")
+            f['catalog/sompz/unsheared/z'] = h5.ExternalLink(bpzfile, "/catalog/bpz/z")
+            f['catalog/sompz/unsheared/redshift_cos'] = h5.ExternalLink(bpzfile, "/catalog/bpz/redshift_cos")
 
-        with h5.File(catfile, 'r+') as f:
+        return
 
-            f['catalog/sompz/pzdata'] = h5.ExternalLink(sompzfile, 'catalog/sompz/pzdata')
-            print('linked file')
+    else:
+        with h5.File(sompzfile, 'r') as fs:
+
+            som_id = fs['catalog/sompz/unsheared/coadd_object_id'][:]
+            cols = list(fs['catalog/sompz/unsheared/'].keys())
+
+            print(cols)
             sys.stdout.flush()
 
-            coadd_id = f['catalog/gold/coadd_object_id'][:]
-            total_length = len(coadd_id)
-            print('read')
-            sys.stdout.flush()
-            
-            iidx = np.argsort(coadd_id)
-            print('sorted')
-            sys.stdout.flush()            
-            cat_idx = coadd_id.searchsorted(som_id, sorter=iidx)
+            with h5.File(catfile, 'r+') as f:
 
-            print('searched')
-            sys.stdout.flush()
-
-            sdtype = np.dtype([(c, fs['catalog/sompz/unsheared/' + c].dtype.name) for c in cols])
-
-            sompzarray = np.zeros((total_length), dtype=sdtype)
-
-            for name in cols:
-                print(name)
+                f['catalog/sompz/pzdata'] = h5.ExternalLink(sompzfile, 'catalog/sompz/pzdata')
+                print('linked file')
                 sys.stdout.flush()
-                sompzarray[name][iidx[cat_idx]] = fs['catalog/sompz/unsheared/' + name][:]
+
+                coadd_id = f['catalog/gold/coadd_object_id'][:]
+                total_length = len(coadd_id)
+                print('read')
+                sys.stdout.flush()
+
+                iidx = np.argsort(coadd_id)
+                print('sorted')
+                sys.stdout.flush()
+                cat_idx = coadd_id.searchsorted(som_id, sorter=iidx)
+
+                print('searched')
+                sys.stdout.flush()
+
+                sdtype = np.dtype([(c, fs['catalog/sompz/unsheared/' + c].dtype.name) for c in cols])
+
+                sompzarray = np.zeros((total_length), dtype=sdtype)
+
+                for name in cols:
+                    print(name)
+                    sys.stdout.flush()
+                    sompzarray[name][iidx[cat_idx]] = fs['catalog/sompz/unsheared/' + name][:]
+
+                    try:
+                        f.create_dataset('catalog/sompz/unsheared/' + name.lower(),
+                                         maxshape=(total_length,),
+                                         shape=(total_length,),
+                                         dtype=fs['catalog/sompz/unsheared/' + name].dtype,
+                                         chunks=(1000000,))
+                    except Exception as e:
+                        print(e)
+                        sys.stdout.flush()
+                        pass
+
+                    f['catalog/sompz/unsheared/' + name.lower()][:] = sompzarray[name]
 
                 try:
-                    f.create_dataset('catalog/sompz/unsheared/' + name.lower(),
-                                     maxshape=(total_length,),
-                                     shape=(total_length,),
-                                     dtype=fs['catalog/sompz/unsheared/' + name].dtype,
-                                     chunks=(1000000,))
-                except Exception as e:
-                    print(e)
-                    sys.stdout.flush()
+                    f['catalog/sompz/unsheared/z'] = h5py.ExternalLink(bpzfile, 'catalog/bpx/unsheared/z')
+                    f['catalog/sompz/unsheared/redshift_cos'] = h5py.ExternalLink(bpzfile, 'catalog/bpx/unsheared/redshift_cos')
+                except:
                     pass
 
-                f['catalog/sompz/unsheared/' + name.lower()][:] = sompzarray[name]
 
-            try:
-                f['catalog/sompz/unsheared/z'] = h5py.ExternalLink(bpzfile, 'catalog/bpx/unsheared/z')
-                f['catalog/sompz/unsheared/redshift_cos'] = h5py.ExternalLink(bpzfile, 'catalog/bpx/unsheared/redshift_cos')
-            except:
-                pass
-                
-                
 if __name__ == '__main__':
 
     mastercat_file = sys.argv[1]
