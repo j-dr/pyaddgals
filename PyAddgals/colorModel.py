29a941f86285d57d67574c5a33339899ba27ba8a
Joseph DeRose <joe.derose13@gmail.com>
2022-11-07 17:01:20 +0900

    stellar mass, ssfr, metallicity from kcorrect templates

--- PyAddgals/colorModel.py
+++ PyAddgals/colorModel.py
@@ -598,3 +598,49 @@ class ColorModel(object):
         sys.stdout.flush()
 
         return sigma5, ranksigma5, redfraction, sed_idx, omag, amag, mag
+
+
+    def get_derived_quantities(self, template_path, coeff, z):
+        """
+        Given a path to a kcorrect template file, as well as
+        kcorrect coefficients for a set of galaxies and return derived
+        quantities
+
+        inputs
+        ------
+        template_path -- str
+            path to kcorrect templates
+        coeff -- (N x N_templates) array
+            array of kcorrect coefficients
+        z -- (N,) array
+            redshifts of galaxies
+
+        returns
+        -------
+        sfr -- (N,) array
+            Array of star formation rates for galaxies
+        met -- (N,) array
+            Array of metallicities for galaxies
+        smass -- (N,) array
+            Array of stellar masses for galaxies
+        """
+
+        # read in relevant template info
+        sfh_tot = fitsio.read(template_path, 12)
+        sfh_met = fitsio.read(template_path, 13)
+        mass_tot = fitsio.read(template_path, 17)
+
+        # get angular diameter distances
+
+        da = self.nbody.cosmo.angularDiameterDistance(z)
+
+        smass = np.dot(mass_tot, coeff.T) * (da * 1e6 / 10) ** 2
+        ssfr = np.dot(coeff, sfh_tot)
+        met = np.dot(coeff, sfh_tot * sfh_met) / ssfr
+        sfr = ssfr * smass[:, np.newaxis]
+
+        # get the values at z_galaxy
+        met = met[:, -1]
+        sfr = sfr[:, -1]
+
+        return sfr, met, smass
